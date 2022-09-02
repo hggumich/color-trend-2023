5fd8af9f1d0b00aee9e01d9b614ed5f70def5608
Hector Garza <hggumich@gmail.com>
2022-09-02 15:05:12 -0400

    Color Trend for 2023

---
+++ main.js
@@ -0,0 +1,25 @@
+import "./style.css";
+
+document.querySelector("#app").innerHTML = `
+  <div class='Title'>
+  <h1>2023 Color Trends
+  </div>
+  <div class='flex-container'>
+  <div class='crumb'>Crumb Cookie</div>
+  <div class='visionary'>Visionary</div>
+  <div class='lilac'>Lilac Breeze</div>
+  <div class='citrus'>Citrus Sachet</div>
+  <div class='briquette'>Briquette</div>
+  <div class='oyster'>Oyster Shell</div>
+  <div class='cloudy'>Cloudy Slate</div>
+  <div class='crystal'>Crystal Oasis</div>
+  <div class='kimono'>Kimono</div>
+  <div class='laurel'>Laurel Wreath</div>
+  <div class='vining'>Vining Ivy</div>
+  <div class='starless'>Starless Sky</div>
+  <div class='silverado'>Silverado</div>
+  <div class='heart'>Heart's Afire</div>
+  <div class='georgian'>Georgian Leather</div>
+  <div class='florentine'>Florentine Lapis</div>
+  </div>
+`;
